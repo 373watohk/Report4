c6cffa5514c101f32d1ed061d2a59fc7a31f205e
hiroyasu kajihara <e185720@ie.u-ryukyu.ac.jp>
2018-11-14 07:41:11 +0900

    first commit

---
+++ src/jp/ac/uryukyu/ie/e185720/Repo4.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e185720;
+
+public class Repo4 {
+    public static void main(String[] args) {
+        String str = null;
+        int moji = str.length();
+        System.out.print(moji);
+    }
+}
