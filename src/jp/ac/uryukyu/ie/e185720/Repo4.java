0ea19b559fe585be199d963fff3a3f0cf2b57d4b
hiroyasu kajihara <e185720@ie.u-ryukyu.ac.jp>
2018-11-14 13:09:27 +0900

    second commit

--- src/jp/ac/uryukyu/ie/e185720/Repo4.java
+++ src/jp/ac/uryukyu/ie/e185720/Repo4.java
@@ -2,8 +2,13 @@ package jp.ac.uryukyu.ie.e185720;
 
 public class Repo4 {
     public static void main(String[] args) {
-        String str = null;
-        int moji = str.length();
-        System.out.print(moji);
+        try {
+            String str = null;
+            int moji = str.length();
+            System.out.print(moji);
+        }catch (NullPointerException n) {
+            System.out.println("例外が発生しました。");
+            System.out.println(n.getMessage());
+        }
     }
 }
